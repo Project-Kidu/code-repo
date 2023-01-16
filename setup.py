089cd7fe07d898045dff6379a03230cbf0a6aff4
gokul-pv <25975535+gokul-pv@users.noreply.github.com>
2023-01-16 08:59:18 +0530

    First version of the code with model logging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Project Kidu",
+    author="Gokul P V",
+    author_email="25975535+gokul-pv@users.noreply.github.com",
+    url="https://github.com/Project-Kidu/code-repo",
+    install_requires=["pytorch-lightning", "hydra-core"],
+    packages=find_packages(),
+)
